012b41233fc9944b5331ef3cffff2abface26755
taeksang12 <arthurk1qks@gmail.com>
2023-11-10 16:12:07 +0900

    11/10 4

--- Upworld/Assets/Script/MainCamera_Action.cs
+++ Upworld/Assets/Script/MainCamera_Action.cs
@@ -6,15 +6,20 @@ public class MainCamera_Action : MonoBehaviour
 {
     public GameObject Target;
 
-    public float offsetX = 0.0f;
-    public float offsetY = 10.0f;
-    public float offsetZ = -10.0f;
+    public float xmove = 0;
+    public float ymove = 0;
+    public float distance = 3;
 
-    public float CameraSpeed = 10.0f;
-    Vector3 TargetPos;
+    //public float offsetX = 0.0f;
+    //public float offsetY = 10.0f;
+    //public float offsetZ = -10.0f;
+
+    //public float CameraSpeed = 10.0f;
+    //Vector3 TargetPos;
 
     void FixedUpdate()
     {
+        /*
         TargetPos = new Vector3(
             Target.transform.position.x + offsetX,
             Target.transform.position.y + offsetY,
@@ -22,5 +27,13 @@ public class MainCamera_Action : MonoBehaviour
             );
 
         transform.position = Vector3.Lerp(transform.position, TargetPos, Time.deltaTime * CameraSpeed);
+
+        */
+
+        xmove += Input.GetAxis("Mouse X");
+        ymove -= Input.GetAxis("Mouse Y");
+        transform.rotation = Quaternion.Euler(ymove, xmove, 0);
+        Vector3 reverseDistance = new Vector3(0.0f, 0.0f, distance);
+        transform.position = Target.transform.position - transform.rotation * reverseDistance;
     }
 }
--- Upworld/Assets/Script/PlayerControl.cs
+++ Upworld/Assets/Script/PlayerControl.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public GameObject Camera;
+    public CharacterController SelectPlayer;
     public float Speed = 10.0f;
 
-    public float rotateSpeed = 10.0f;
+    private float Gravity = 10.0f;
+    private Vector3 MoveDir = Vector3.zero;
+    private bool JumpButtonPressed = false;
+    private bool FlyingMode = false;
+
+    //public float rotateSpeed = 10.0f;
 
     public float JumpForce = 5.0f;
 
-    private bool isGround = true;
+    //private bool isGround = true;
 
     Rigidbody body;
 
@@ -30,10 +37,65 @@ public class PlayerControl : MonoBehaviour
 
     void FixedUpdate()
     {
-        Move();
-        Jump();
+        //Move();
+        //Jump();
+
+        if (SelectPlayer == null) return;
+
+        if (Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            var offset = Camera.transform.forward;
+            offset.y = 0;
+            transform.LookAt(SelectPlayer.transform.position + offset);
+        }
+
+        if (SelectPlayer.isGrounded)
+        {
+            MoveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            MoveDir = SelectPlayer.transform.TransformDirection(MoveDir);
+
+            MoveDir *= Speed;
+
+            if (JumpButtonPressed == false && Input.GetButton("Jump"))
+            {
+                SelectPlayer.transform.rotation = Quaternion.Euler(0, 45, 0);
+                JumpButtonPressed = true;
+                MoveDir.y = JumpForce;
+            }
+        }
+        else
+        {
+            if (MoveDir.y < 0 && JumpButtonPressed == false && Input.GetButton("Jump"))
+            {
+                FlyingMode = true;
+            }
+
+            if (FlyingMode)
+            {
+                JumpButtonPressed = true;
+
+                MoveDir.y *= 0.95f;
+
+                if (MoveDir.y > -1) MoveDir.y = -1;
+
+                MoveDir.x = Input.GetAxis("Horizontal");
+                MoveDir.z = Input.GetAxis("Vertical");
+            }
+            else
+                MoveDir.y -= Gravity * Time.deltaTime;
+        }
+
+        if (!Input.GetButton("Jump"))
+        {
+            JumpButtonPressed = false;
+            FlyingMode = false;
+        }
+
+        SelectPlayer.Move(MoveDir * Time.deltaTime);
     }
 
+    /*
     void Move()
     {
         h = Input.GetAxis("Horizontal");
@@ -64,6 +126,15 @@ public class PlayerControl : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGround = true;
+        }
+    }
+    */
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Lava")
+        {
+            Destroy(gameObject);
         }
     }
 }
